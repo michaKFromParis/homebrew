857f4b870824f54ddc74c57216f855db09351879
Michel Courtine <michaK@ivoltage.me>
2015-07-02 19:24:53 +0200

    Added support for setting MACOSX_DEPLOYMENT_TARGET to libgit2

--- Library/Formula/libgit2.rb
+++ Library/Formula/libgit2.rb
@@ -13,6 +13,10 @@ class Libgit2 < Formula
   end
 
   option :universal
+  option "with-10.6", "support OSX 10.6 and up"
+  option "with-10.7", "support OSX 10.7 and up"
+  option "with-10.8", "support OSX 10.8 and up"
+  option "with-10.9", "support OSX 10.9 and up"
 
   depends_on "cmake" => :build
   depends_on "libssh2" => :optional
@@ -26,6 +30,11 @@ class Libgit2 < Formula
       ENV.universal_binary
       args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
     end
+
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = '10.6' if build.with? "10.6"
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = '10.7' if build.with? "10.7"
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = '10.8' if build.with? "10.8"
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = '10.9' if build.with? "10.9"
 
     mkdir "build" do
       system "cmake", "..", *args
